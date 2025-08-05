8c8baa1a68de9c128ddfc64ffca6b94e5e21f3f1
LouiseDavise <louizkwok@gmail.com>
2025-08-06 00:43:54 +0800

    SignUp modification

--- dating_chatbot/chatbot/form.py
+++ dating_chatbot/chatbot/form.py
@@ -3,35 +3,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class Login(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'John Doe',
-        'class': 'w-full bg-gray-800/60 placeholder-gray-500 px-10 py-2.5 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 ring-indigo-500',
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': '••••••••',
-        'class': 'w-full bg-gray-800/60 placeholder-gray-500 px-10 py-2.5 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 ring-indigo-500',
-    }))
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
 
 class UserRegistration(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'placeholder': 'you@factory.com',
-        'class': 'w-full bg-gray-800/60 placeholder-gray-500 px-10 py-2.5 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 ring-indigo-500',
-    }))
-
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Jane Doe',
-        'class': 'w-full bg-gray-800/60 placeholder-gray-500 px-10 py-2.5 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 ring-indigo-500',
-    }))
-
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': '••••••••',
-        'class': 'w-full bg-gray-800/60 placeholder-gray-500 px-10 py-2.5 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 ring-indigo-500',
-    }))
-
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': '••••••••',
-        'class': 'w-full bg-gray-800/60 placeholder-gray-500 px-10 py-2.5 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 ring-indigo-500',
-    }))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'autocomplete': 'email'}))
 
     class Meta:
         model = User
