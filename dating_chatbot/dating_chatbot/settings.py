3bfeca2d11561df164b545c82c68cd8ce11aeff4
delfinodjaja <delfinojaconiahdjaja@gmail.com>
2025-08-09 15:33:31 +0700

    fixed database and form

--- dating_chatbot/dating_chatbot/settings.py
+++ dating_chatbot/dating_chatbot/settings.py
@@ -42,7 +42,14 @@ INSTALLED_APPS = [
     'corsheaders',
     'chatbot',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
